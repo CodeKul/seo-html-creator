77a1a83b55189d587888b6dbeefe323eac894b4f
aniruddha <aniruddha.kudalkar@gmail.com>
2017-03-14 19:22:57 +0530

    ws : resty

--- src/main/java/com/melayer/seo/Main.java
+++ src/main/java/com/melayer/seo/Main.java
@@ -1,6 +1,11 @@
 package com.melayer.seo;
 
 import com.melayer.domain.Article;
+import us.monoid.json.JSONException;
+import us.monoid.web.JSONResource;
+import us.monoid.web.Resty;
+
+import java.io.IOException;
 
 /**
  * Created by aniruddha on 14/3/17.
@@ -14,5 +19,15 @@ public class Main {
         article.setName("Android development in pune");
         HtmlGenerator generator = new HtmlGenerator();
         generator.saveArticle(article);
+
+        try {
+            JSONResource res = new Resty().json("http://ipinfo.io");
+            System.out.printf("\n Json is %s", res.toObject().toString());
+            System.out.printf("\n Ip is - %s", res.object().getString("ip"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
